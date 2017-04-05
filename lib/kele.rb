ad6d0bc63fb0b9de213c39f7acebddb9c817e1c9
xortiz <xavier.ortiz.ch@gmail.com>
2017-04-04 22:01:43 -0400

    wow. What a goose chase!

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,6 +1,16 @@
+require 'httparty'
 class Kele
-  def initialize (username, password)
-    @username = username
-    @password = password
+  include HTTParty
+
+  # @headers = { content_type: 'application/json' }
+
+  def initialize(email, password)
+
+    @options = { email: email, password: password }
   end
+
+  def get_auth_token
+    puts self.class.post('https://www.bloc.io/api/v1/sessions', query: @options)
+  end
+
 end
