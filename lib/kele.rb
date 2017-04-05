3ba0126c0b897737231b598fc006762eee38b3b2
xortiz <xavier.ortiz.ch@gmail.com>
2017-04-04 22:22:12 -0400

    previous commit was on checkpoint branch, realized there was no master branch. Created master branch, adding this extra commit to checkpoint branch to add a new commit and be able to create a PR to master

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,23 +1,23 @@
 require 'httparty'
 class Kele
-  include HTTParty
+	include HTTParty
 
-  # @headers = { content_type: 'application/json' }
+	# @headers = { content_type: 'application/json' }
 
-  def initialize(email, password)
+	def initialize(email, password)
 
-    @options = { email: email, password: password }
-  end
+		@options = { email: email, password: password }
+	end
 
-  def get_auth_token
-    @stored = self.class.post('https://www.bloc.io/api/v1/sessions', query: @options)
+	def get_auth_token
+		@stored = self.class.post('https://www.bloc.io/api/v1/sessions', query: @options)
 
-    begin
-      @stored.inspect
-    rescue => e
-      puts "Rescued #{e.inspect}"
-    end
+		begin
+			@stored.inspect
+		rescue => e
+			puts "Rescued #{e.inspect}"
+		end
 
-  end
+	end
 
 end
